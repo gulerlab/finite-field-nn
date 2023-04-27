c0f1e41ee78ad7a01e04241082792fb5553888d0
umityigitbsrn <umityigitbsrn@gmail.com>
2023-04-26 18:31:00 -0700

    updated .gitignore real to ff, ff to real and ff truncation functions are implemented some update in simple_network.py

--- simple_network.py
+++ simple_network.py
@@ -1,20 +1,35 @@
 import torch
 import torch.nn as nn
+from utils import to_real_domain, to_finite_field_domain, finite_field_truncation
 
-class CustomNet():
-    def __init__(self, input_vector, hidden_layer_size=64, num_classes=10, device=None) -> None:
-        super.__init__()
-        self.input_vector = input_vector
+
+class VectorizedScaledNet:
+    def __init__(self, scale_x, scale_w, input_vector_size=784, hidden_layer_size=64, num_classes=10, device=None,
+                 verbose=True) -> None:
+        self.input_vector_size = input_vector_size
         self.hidden_layer_size = hidden_layer_size
         self.num_classes = num_classes
+        self.device = device
+        self.verbose = verbose
+
+        self.__to_device()
         self.__init_weight()
 
     def __init_weight(self):
-        self.weight_1 = torch.empty(self.input_vector.size(0), self.hidden_layer_size)
+        self.weight_1 = torch.empty(self.input_vector_size, self.hidden_layer_size)
         self.weight_2 = torch.empty(self.hidden_layer_size, self.num_classes)
         nn.init.kaiming_normal_(self.weight_1)
         nn.init.kaiming_normal_(self.weight_2)
-    
+
+    def __to_device(self):
+        self.weight_1 = self.weight_1.to(self.device)
+        self.weight_2 = self.weight_2.to(self.device)
+
+        # if self.verbose:
+
+    def __init_scale(self):
+        pass
+
     def __criterion(self):
         pass
 
@@ -26,6 +41,6 @@ class CustomNet():
 
     def __scaled_backward(self):
         pass
-    
+
     def train(self):
         pass
---
+++ utils.py
@@ -0,0 +1,34 @@
+import torch
+from torch import Tensor
+import numpy as np
+
+
+def to_finite_field_domain(real: Tensor, quantization_bit: int, prime: int) -> Tensor:
+    scaled_real = real * (2 ** quantization_bit)
+    int_domain = torch.round(scaled_real)
+    negative_mask = int_domain < 0
+    int_domain[negative_mask] = int_domain[negative_mask] + prime
+    finite_field_domain = int_domain.type(torch.long)
+    return finite_field_domain
+
+
+def to_real_domain(finite_field: Tensor, quantization_bit: int, prime: int) -> Tensor:
+    threshold = (prime - 1) / 2
+    negative_mask = finite_field > threshold
+    finite_field[negative_mask] = finite_field[negative_mask] - prime
+    real_domain = finite_field.type(torch.float)
+    real_domain = real_domain / (2 ** quantization_bit)
+    return real_domain
+
+
+def finite_field_truncation(finite_field: Tensor, scale_down: int) -> Tensor:
+    real_domain = finite_field.type(torch.float)
+    real_domain = real_domain / (2 ** scale_down)
+    real_domain_floor = torch.floor(real_domain)
+
+    zero_distributions = (real_domain - real_domain_floor).to('cpu')
+    zero_distributions.apply_(lambda x: np.random.choice([0, 1], 1, p=[1 - x, x])[0])
+    zero_distributions = zero_distributions.to(finite_field.device)
+
+    finite_field_domain = (real_domain_floor + zero_distributions).type(torch.long)
+    return finite_field_domain
