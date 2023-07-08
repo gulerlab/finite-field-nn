d6653de7cb43ba415984efcf52179d1166c6fff3
umityigitbsrn <umityigitbsrn@gmail.com>
2023-07-08 12:09:19 +0300

    bash update

--- nets/finite_field_net/finite_field_training_pipeline_cifar.sh
+++ nets/finite_field_net/finite_field_training_pipeline_cifar.sh
@@ -10,7 +10,7 @@
 #SBATCH --job-name="CIFAR10 Finite Field Domain Training"
 #SBATCH -p cpu # You could pick other partitions for other jobs
 #SBATCH --wait-all-nodes=1  # Run once all resources are available
-#SBATCH --output=int_cifar10_output_%j-%N.txt # logging per job and per host in the current directory. Both stdout and stderr are logged.
+#SBATCH --output=finite_field_cifar10_output_%j-%N.txt # logging per job and per host in the current directory. Both stdout and stderr are logged.
 
 conda activate ff-net
 python finite_field_training_pipeline_cifar.py
--- nets/finite_field_net/finite_field_training_pipeline_mnist.sh
+++ nets/finite_field_net/finite_field_training_pipeline_mnist.sh
@@ -10,7 +10,7 @@
 #SBATCH --job-name="MNIST Finite Field Domain Training"
 #SBATCH -p cpu # You could pick other partitions for other jobs
 #SBATCH --wait-all-nodes=1  # Run once all resources are available
-#SBATCH --output=int_mnist_output_%j-%N.txt # logging per job and per host in the current directory. Both stdout and stderr are logged.
+#SBATCH --output=field_mnist_output_%j-%N.txt # logging per job and per host in the current directory. Both stdout and stderr are logged.
 
 conda activate ff-net
 python finite_field_training_pipeline_mnist.py
