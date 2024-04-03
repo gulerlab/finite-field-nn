6aa04b6cd7e746b9bc62a34fe64fa42c4617d4d4
umityigitbsrn <umityigitbsrn@gmail.com>
2024-04-03 13:58:29 -0700

    some small updates and type s

--- ff_clover_main.py
+++ ff_clover_main.py
@@ -45,6 +45,8 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--mode', type=str, help='experiment type')
     parser.add_argument('-p', '--prime', dest='prime', type=int, default=((2**26) - 5))
     parser.add_argument('-g', '--gamma', dest='gamma', type=int, default=8)
+    parser.add_argument('-lr', '--learning-rate', dest='learning_rate', type=int, default=10)
+    parser.add_argument('-sg', '--second-gamma', dest='second_gamma', type=int)
     args = parser.parse_args()
 
     experiment_now = datetime.now()
@@ -58,45 +60,51 @@ if __name__ == '__main__':
     logging.basicConfig(filename=log_file_path, encoding='utf-8', level=logging.DEBUG)
     logging.info('PRIME: {}'.format(args.prime))
     logging.info('GAMMA: {}'.format(args.gamma))
+    if args.second_gamma:
+        logging.info('WEIGHT GAMMA: {}'.format(args.second_gamma))
+        second_gamma = args.secong_gamma
+    else:
+        second_gamma = args.gamma
+    logging.info('LEARNING RATE: {}'.format(args.learning_rate))
     net = None
     if args.mode == 'fashion-mnist':
-        scaled_input, scale_weight, learning_rate, prime = args.gamma, args.gamma, 7, args.prime
+        scaled_input, scale_weight, learning_rate, prime = args.gamma, second_gamma, args.learning_rate, args.prime
         feature_size, hidden_layer_size, num_class = 784, 128, 10
         num_epoch, batch_size = 1, 256
         net = FiniteFieldClover(scaled_input, scale_weight, learning_rate, prime,
                                 feature_size=feature_size, hidden_layer_size=hidden_layer_size, num_classes=num_class)
         net.train(num_epoch, batch_size)
     elif args.mode == 'mnist':
-        scaled_input, scale_weight, learning_rate, prime = args.gamma, args.gamma, 7, args.prime
+        scaled_input, scale_weight, learning_rate, prime = args.gamma, second_gamma, args.learning_rate, args.prime
         feature_size, hidden_layer_size, num_class = 784, 128, 10
         num_epoch, batch_size = 1, 256
         net = FiniteFieldClover(scaled_input, scale_weight, learning_rate, prime,
                                 feature_size=feature_size, hidden_layer_size=hidden_layer_size, num_classes=num_class)
         net.train_mnist(num_epoch, batch_size)
     elif args.mode == 'mnist-v2':
-        scaled_input, scale_weight, learning_rate, prime = args.gamma, args.gamma, 7, args.prime
+        scaled_input, scale_weight, learning_rate, prime = args.gamma, second_gamma, args.learning_rate, args.prime
         feature_size, hidden_layer_size, num_class = 784, 128, 10
         num_epoch, batch_size = 300, 256
         net = FiniteFieldClover(scaled_input, scale_weight, learning_rate, prime,
                                 feature_size=feature_size, hidden_layer_size=hidden_layer_size, num_classes=num_class)
         net.train_mnist_v2(num_epoch, batch_size)
     elif args.mode == 'cifar10':
-        scaled_input, scale_weight, learning_rate, prime = args.gamma, args.gamma, 10, args.prime
+        scaled_input, scale_weight, learning_rate, prime = args.gamma, second_gamma, args.learning_rate, args.prime
         feature_size, hidden_layer_size, num_class = 3072, 128, 10
         num_epoch, batch_size = 1, 256
         net = FiniteFieldClover(scaled_input, scale_weight, learning_rate, prime,
                                 feature_size=feature_size, hidden_layer_size=hidden_layer_size, num_classes=num_class)
         net.train_cifar10(num_epoch, batch_size)
     elif args.mode == 'cifar10-vgg':
-        scaled_input, scale_weight, learning_rate, prime = args.gamma, args.gamma, 10, args.prime
+        scaled_input, scale_weight, learning_rate, prime = args.gamma, second_gamma, args.learning_rate, args.prime
         feature_size, hidden_layer_size, num_class = 25088, 128, 10
         num_epoch, batch_size = 1, 256
         net = FiniteFieldClover(scaled_input, scale_weight, learning_rate, prime,
                                 feature_size=feature_size, hidden_layer_size=hidden_layer_size, num_classes=num_class)
         net.train_vgg_cifar10(num_epoch, batch_size)
     elif args.mode == 'cifar10-vgg-v2':
-        scaled_input, scale_weight, learning_rate, prime = args.gamma, args.gamma, 10, args.prime
-        feature_size, hidden_layer_size, num_class = 25088, 128, 10
+        scaled_input, scale_weight, learning_rate, prime = args.gamma, second_gamma, args.learning_rate, args.prime
+        feature_size, hidden_layer_size, num_class = 25088, 128, args.learning_rate
         num_epoch, batch_size = 300, 256
         net = FiniteFieldClover(scaled_input, scale_weight, learning_rate, prime,
                                 feature_size=feature_size, hidden_layer_size=hidden_layer_size, num_classes=num_class)
